e0731558762b77077906a1e75cbfdcfabb5fa5c9
songhengqiang <15292242950@163.com>
2025-01-16 23:59:04 +0800

    first

---
+++ test.py
@@ -0,0 +1,9 @@
+# codecs是Python的一个标准库，它提供了对各种字符编码的读取和写入操作的支持
+import codecs
+
+# 定义一个包含中文字符的Unicode字符串
+text = u'hello, 我喜欢python'
+text2 = 'hello, 我喜欢python！'
+
+print(text)
+print(text2)
---
+++ 序列Demo.py
@@ -0,0 +1,13 @@
+s='hello world'
+if 'h' in s:
+    print('h包含在s中')
+# 函数
+print(len(s))
+
+# 序列对象的方法，使用序列的名称，打点调用
+# 序列s中第一次出现元素x的位置 2
+print(s.index('l'))
+
+# 序列s中出现x的总次数
+print(s.count('l'))
+
